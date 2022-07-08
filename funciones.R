8ca5c1233772ea1ce40c58ee7c479c7e095b7404
rsalgadog <ricardo.salgado@unah.edu.hn>
2022-07-08 08:26:02 -0600

    Se agregaron nuevos archivos y se creó el .gitignore

---
+++ funciones.R
@@ -0,0 +1,15 @@
+#Trabajando con funciones 
+#function(){}
+
+#Creando primera funcion 
+suma <- function(x,y){
+  x+y
+  
+}
+suma(2,5)
+suma(5,2)
+a <- c(2, 5, -3, 10, 4, 9,19)
+b <- c(-2, -1, 11, 5, 2, 7, 4)
+c <- c(1, 5)
+suma(a,b)
+suma(a,c)
